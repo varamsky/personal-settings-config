2b7336b7629b4b32bc2241d96d941a936f5ef8cf
varamsky <shubhamkumar5772@gmail.com>
2020-05-31 15:27:45 +0530

    Added git to flutter_automate shell script. Added python script to run quotes on terminal startup(experimantal)

---
+++ quotes/run_quote.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import json
+import os
+import random
+import unicodedata
+
+file_name = '/mnt/6C0801E20801ABE0/IdeaProjects/personal-settings-config/quotes/quotes.json'
+
+with open(file_name) as file:
+	data = json.load(file)
+	data = data['quotes-data']
+	random_index = random.randint(0, len(data)-1)
+	#print(random_index)
+	quote = data[random_index]['quote']
+	author = data[random_index]['author']
+	print(" \""+quote+"\"")
+	print("  - "+author)
+
